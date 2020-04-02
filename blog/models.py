c2202cfc0332340fd6ba7fb322f7fdc1bb0275b2
Kevin Yiu-Wah Cheung <kcheung8@buffalo.edu>
2020-04-01 23:46:03 -0400

    3rd commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(default=datetime.now)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
