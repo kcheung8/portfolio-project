26be54b1c4c72457ab3a7327cd80ae83f8b389cf
Kevin Yiu-Wah Cheung <kcheung8@buffalo.edu>
2020-04-04 23:00:40 -0400

    swag

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,10 +115,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
